9139c1d51c14ec77ba2423785648a8eefa96e5aa
tanyaroy1541 <tanyaroy1541@gmail.com>
2022-07-08 05:26:47 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddbooksComponent } from './addbooks/addbooks.component';
+import { SearchbookComponent } from './searchbook/searchbook.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddbooksComponent
+    AddbooksComponent,
+    SearchbookComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/searchbook/searchbook.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchbook',
+  templateUrl: './searchbook.component.html',
+  styleUrls: ['./searchbook.component.css']
+})
+export class SearchbookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
